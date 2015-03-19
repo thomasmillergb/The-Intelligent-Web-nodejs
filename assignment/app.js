6db75515f51ec6f3c86d453aad47dd4b9d4d21f5
killermillergb <thomasmillergb@outlook.com>
2015-03-19 20:16:23 +0000

    nice tidy up of apps

--- assignment/app.js
+++ assignment/app.js
@@ -29,7 +29,9 @@ app.use(express.session({
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
+////////////////////////////////////////////////
+////////////////Routers////////////////////////
+////////////////////////////////////////////////
 app.get('/', routes.index);
 //app.get('/auth/twitter/callback', routes.authTwitter);
 app.get('/endoftwit', routes.endoftwit);
@@ -40,76 +42,12 @@ http.createServer(app).listen(app.get('port'), function() {
 });
 
 var util = require('util');
-
-var OAuth = require('oauth').OAuth;
-
-var oa = new OAuth(
-  "https://api.twitter.com/oauth/request_token",
-  "https://api.twitter.com/oauth/access_token",
-  "NlT41DmogCgb5C6PsgogvHy29",
-  "4e0sav0ciNSlafDjMWjQKXAQXCmxAC3vfTQv9TuB5LEiJPP905",
-  "1.0",
-  "http://127.0.0.1:3000/auth/twitter/callback",
-  "HMAC-SHA1"
-);
-app.get('/auth/twitter', function(req, res) {
-  oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
-    if (error) {
-      console.log(error);
-      res.send("yeah no. didn't work.")
-    } else {
-      req.session.oauth = {};
-      req.session.oauth.token = oauth_token;
-      console.log('oauth.token: ' + req.session.oauth.token);
-      req.session.oauth.token_secret = oauth_token_secret;
-      console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
-      res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token)
-    }
-  });
-});
-
-var twitter = require('ntwitter');
-app.get('/auth/twitter/callback', function(req, res, next) {
-  if (req.session.oauth) {
-    req.session.oauth.verifier = req.query.oauth_verifier;
-    var oauth = req.session.oauth;
-
-    oa.getOAuthAccessToken(oauth.token, oauth.token_secret, oauth.verifier,
-      function(error, oauth_access_token, oauth_access_token_secret, results) {
-        if (error) {
-          console.log(error);
-          res.send("yeah something broke.");
-        } else {
-          req.session.oauth.access_token = oauth_access_token;
-          req.session.oauth.access_token_secret = oauth_access_token_secret;
-          //console.log(results);
-          //console.log(req);
-          var twit = new twitter({
-            consumer_key: "NlT41DmogCgb5C6PsgogvHy29",
-            consumer_secret: "4e0sav0ciNSlafDjMWjQKXAQXCmxAC3vfTQv9TuB5LEiJPP905",
-            access_token_key: req.session.oauth.access_token,
-            access_token_secret: req.session.oauth.access_token_secret
-          });
+var OAuthTwitter = require('./oAuth/twitter.js');
+OAuthTwitter(app);
 
 
-          twit
-            .verifyCredentials(function(err, data) {
-              //console.log(err, data);
-            })
-            .updateStatus('Test tweet from ntwitter/' + twitter.VERSION,
-              function(err, data) {
-                console.log(err, data?data.toString():"");
-                //res.redirect('/');
-                res.render('endoftwit', { title: 'Social Tracker', oauth:oauth});
-              }
-          );
 
-        }
-      }
-    );
-  } else
-    next(new Error("you're not supposed to be here."))
-});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
