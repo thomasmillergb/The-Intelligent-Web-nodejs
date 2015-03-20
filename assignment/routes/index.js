20a9dcd82c9f1a967295d48a87a0f125ee820e22
killermillergb <thomasmillergb@outlook.com>
2015-03-20 05:24:54 +0000

    streams twiiter info

--- assignment/routes/index.js
+++ assignment/routes/index.js
@@ -2,25 +2,31 @@
  * GET home page.
  */
 
-
 var twitter = require('ntwitter');
 var io = require('socket.io').listen(3001, {log: false});
+
 exports.index = function (req, res) {
-    res.render('index', { title: 'Express' });
+    
+    res.render('index', { title: 'Scoial Stalker +' });
     if (req.session.oauth) {
         InitStream(req.session);
     }
+    else
+    {
+
+        //res.render('index', { title: 'Express', name: "nope" });
+    }
+
 };
 var isActive = false;
 var InitStream = function (session) {
-	console.log("session");
-	console.log(session);
     var twit = new twitter({
         consumer_key: "NlT41DmogCgb5C6PsgogvHy29",
         consumer_secret: "4e0sav0ciNSlafDjMWjQKXAQXCmxAC3vfTQv9TuB5LEiJPP905",
         access_token_key: session.oauth.access_token,
         access_token_secret: session.oauth.access_token_secret
     });
+
 
     twit
         .verifyCredentials(function (err, data) {
@@ -34,11 +40,26 @@ var InitStream = function (session) {
 
 
     if (!isActive) {
-    twit.stream('statuses/sample', function(stream) {
-    stream.on('data', function (data) {
-        console.log(data);
-        });
-        });
+        console.log('init Stream');
+        twit.stream('statuses/filter',{track: ['pie']},function (stream) {
+                stream.on('data', function (data) {
+                    //console.log(data);
+                    console.log(data.user.screen_name + " : " + data.text);
+                    io.sockets.emit('newTwitt', data);
+                    // throw  new Exception('end');
+                });
+                stream.on('end', function (end) {
+                    console.log('end stream', end.toString());
+                    isActive = false;
+                    InitStream(session);
+                });
+                stream.on('destroy', function (destroy) {
+                    console.log('destroy stream', destroy.toString());
+                    isActive = false;
+                    InitStream(session);
+                });
+            }
+        );
         isActive = true;
     }
 };
